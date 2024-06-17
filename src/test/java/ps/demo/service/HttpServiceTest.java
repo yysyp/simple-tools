2808b7f4febbeed9d6fa5476e22b52e1d71ee9d0
yp <yysyp@foxmail.com>
2024-06-18 07:55:14 +0800

    add awaitility

--- src/test/java/ps/demo/service/HttpServiceTest.java
+++ src/test/java/ps/demo/service/HttpServiceTest.java
@@ -1,10 +1,16 @@
 package ps.demo.service;
 
+import cn.hutool.core.lang.Console;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.jayway.jsonpath.JsonPath;
 import lombok.extern.slf4j.Slf4j;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +19,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @SpringBootTest
@@ -51,6 +60,26 @@ public class HttpServiceTest {
         {"basic": [{"name": "xiaoh", "time": "2024-01-01T00:00:00Z", "ver": 1}], "user": "555", "data": {"x": 123, "y": "hhi"}}
         """, "$.basic[0].name");
         log.info("==> str = {}", str);
+
+    }
+
+    @Test
+    void testAwait() {
+        Awaitility.await()
+                .timeout(5, TimeUnit.MINUTES)
+                .pollDelay(1, TimeUnit.MINUTES)
+                .pollInterval(Duration.ofSeconds(5))
+                .until(() -> {
+                    Console.log("Await ...");
+                    return RandomUtils.nextBoolean();
+                });
+
+    }
+
+    @Test
+    void getSystemProperty() {
+        final String pass = System.getProperty("pass");
+        Preconditions.checkArgument(StringUtils.isNotEmpty(pass), "Please add Env variable -Dpass=xxx ");
 
     }
 
